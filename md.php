62a8eea33ebe6f09ad6f8a91e41e57909ef5d52f
無常 <3142324836@qq.com>
2023-06-28 02:34:49 +0800

    Add html div

--- md.php
+++ md.php
@@ -38,6 +38,6 @@ echo '<link rel="stylesheet" type="text/css" href="styles/github-markdown.css">'
 echo '<link rel="stylesheet" type="text/css" href="styles/highlight-js.css">';
 echo '<link rel="stylesheet" type="text/css" href="styles/katex.min.css">';
 echo '</head>';
-echo '<body class="dark"><article class="markdown-body entry-content container-lg" itemprop="text">';
+echo '<body class="dark"><div class="content"><article class="markdown-body entry-content container-lg" itemprop="text">';
 echo $html_article_body;
-echo '</article></body></html>';
+echo '</article></div></body></html>';
